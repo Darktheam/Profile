0290bc27f741841f179318dbc9926e267c2f4d37
Darktheam <100502037+Darktheam@users.noreply.github.com>
2025-03-04 18:08:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function c(){
+var e = document.getElementById("ni").value;
+alert("hello "+ e);
+var b=confirm('are you sure');
+alert(b)
+}
+function a(){
+var b=prompt('input your name');
+if(b.length==0){a()}
+else{alert('hello, '+b);}
+}
+function b(){
+window.open('C:\\Users\\ARYAN\\Desktop\\html\\image.jpg','blank')
+}
